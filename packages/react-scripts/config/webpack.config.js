4193d0547808fe22573d0e2f3d70571030828392
Valerii Gusev <valerii.gusev@cognite.com>
2019-08-23 08:44:16 +0200

    Enable babel-plugin-styled-components for development
    This plugin has been removed during migration

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -419,7 +419,9 @@ module.exports = function(webpackEnv) {
                       },
                     },
                   ],
-                ],
+                  !isEnvProduction &&
+                    require.resolve('babel-plugin-styled-components'),
+                ].filter(Boolean),
                 // This is a feature of `babel-loader` for webpack (not Babel itself).
                 // It enables caching results in ./node_modules/.cache/babel-loader/
                 // directory for faster rebuilds.
